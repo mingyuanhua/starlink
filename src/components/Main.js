0c176ce949d84ea667ef6fdc502d8d8af2042753
mingyuanhua <m.y.hua@hotmail.com>
2022-11-20 01:57:56 +0800

    Add world map

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,71 +1,85 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Row, Col } from 'antd';
-import axios from "axios";
+import axios from 'axios';
 
 import SatSetting from './SatSetting';
 import SatelliteList from './SatelliteList';
-import { SAT_API_KEY, BASE_URL, NEARBY_SATELLITE, STARLINK_CATEGORY } from "../constants";
 import WorldMap from './WorldMap';
-
+import {
+  SAT_API_KEY,
+  BASE_URL,
+  NEARBY_SATELLITE,
+  STARLINK_CATEGORY,
+} from '../constants';
 
 class Main extends Component {
-    state = {
-        setting: {},
-        satInfo: {},
-        satList: [],
-        isLoadingList: false
-    }
+  state = {
+    setting: {},
+    satInfo: {},
+    satList: [],
+    isLoadingList: false,
+  };
 
-    showNearbySatellite = (setting) => {
+  showNearbySatellite = (setting) => {
+    console.log('show nearby');
+    this.setState({
+      setting: setting,
+    });
+    this.fetchSatellite(setting);
+  };
+
+  fetchSatellite = (setting) => {
+    console.log('fetching');
+    const { latitude, longitude, elevation, altitude } = setting;
+    const url = `${BASE_URL}/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+    this.setState({
+      isLoadingList: true,
+    });
+
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
         this.setState({
-            setting: setting
-        })
-        this.fetchSatellite(setting);
-    }
+          satInfo: res.data,
+          isLoadingList: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          isLoadingList: true,
+        });
+        console.log('err in fetch satellite -> ', error);
+      });
+  };
 
-    fetchSatellite= (setting) => {
-        const {latitude, longitude, elevation, altitude} = setting;
-        const url = `${BASE_URL}/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+  showMap = (selected) => {
+    this.setState((preState) => ({
+      ...preState,
+      satList: [...selected],
+    }));
+  };
 
-   this.setState({
-       isLoadingList: true
-   });
+  render() {
+    const { satInfo, isLoadingList, satList, setting } = this.state;
 
-   axios.get(url)
-       .then(response => {
-           console.log(response.data)
-           this.setState({
-               satInfo: response.data,
-               isLoadingList: false
-           })
-       })
-       .catch(error => {
-           console.log('err in fetch satellite -> ', error);
-       })
-    }
-
-    showMap = () => {
-        console.log('show on the map');
-    }
-
-    render() {
-        const { satInfo } = this.state;
-        return (
-            <Row className="main">
-              <Col span={8} className="left-side">
-                <SatSetting onShow={this.showNearbySatellite} />
-                <SatelliteList
-                   satInfo={satInfo}
-                   isLoad={this.state.isLoadingList}
-                   onShowMap={this.showMap}
-                />
-              </Col>
-              <Col span={16} className="right-side">
-                <WorldMap />
-              </Col>
-            </Row>
-        );
-    }
+    return (
+      <Row className="main">
+        <Col span={8} className="left-side">
+          <SatSetting onShow={this.showNearbySatellite} />
+          <SatelliteList
+            satInfo={satInfo}
+            isLoad={isLoadingList}
+            onShowMap={this.showMap}
+          />
+        </Col>
+        <Col span={16} className="right-side">
+          <WorldMap satData={satList} observerData={setting} />
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Main;
--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -1,82 +1,86 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { List, Avatar, Button, Checkbox, Spin } from 'antd';
-import satellite from "../assets/images/satellite.svg";
+import satellite from '../assets/images/satellite.svg';
 
 class SatelliteList extends Component {
-    constructor(){
-        super();
-        this.state = {
-            selected: []
-        };
+  constructor() {
+    super();
+    this.state = {
+      selected: [],
+      isLoad: false,
+    };
+  }
+
+  onChange = (e) => {
+    const { dataInfo, checked } = e.target;
+    const { selected } = this.state;
+    const list = this.addOrRemove(dataInfo, checked, selected);
+    this.setState({ selected: list });
+  };
+
+  addOrRemove = (item, status, list) => {
+    const found = list.some((entry) => entry.satid === item.satid);
+    if (status && !found) {
+      list = [...list, item];
     }
 
-    onChange = e => {
-        const { dataInfo, checked } = e.target;
-        const { selected } = this.state;
-        const list = this.addOrRemove(dataInfo, checked, selected);
-        this.setState({ selected: list })
+    if (!status && found) {
+      list = list.filter((entry) => {
+        return entry.satid !== item.satid;
+      });
     }
+    return list;
+  };
 
-    addOrRemove = (item, status, list) => {
-        const found = list.some( entry => entry.satid === item.satid);
-        if(status && !found){
-            list=[...list, item]
-        }
+  onShowSatMap = () => {
+    this.props.onShowMap(this.state.selected);
+  };
 
-        if(!status && found){
-            list = list.filter( entry => {
-                return entry.satid !== item.satid;
-            });
-        }
-        return list;
-    }
+  render() {
+    const satList = this.props.satInfo ? this.props.satInfo.above : [];
+    const { isLoad } = this.props;
+    const { selected } = this.state;
 
-    onShowSatMap = () =>{
-        this.props.onShowMap(this.state.selected);
-    }
+    return (
+      <div className="sat-list-box">
+        <Button
+          className="sat-list-btn"
+          size="large"
+          disabled={selected.length === 0}
+          onClick={this.onShowSatMap}
+        >
+          Track
+        </Button>
+        <hr />
 
-    render() {
-        const satList = this.props.satInfo ? this.props.satInfo.above : [];
-        const { isLoad } = this.props;
-        const { selected } = this.state;
-
-        return (
-            <div className="sat-list-box">
-                <Button className="sat-list-btn"
-                        type="primary"
-                        disabled={ selected.length === 0}
-                        onClick={this.onShowSatMap}
-                >Track</Button>
-                <hr/>
-
-                {
-                    isLoad ?
-                        <div className="spin-box">
-                            <Spin tip="Loading..." size="large" />
-                        </div>
-                        :
-                        <List
-                            className="sat-list"
-                            itemLayout="horizontal"
-                            size="small"
-                            dataSource={satList}
-                            renderItem={item => (
-                                <List.Item
-                                    actions={[<Checkbox dataInfo={item} onChange={this.onChange}/>]}
-                                >
-                                    <List.Item.Meta
-                                        avatar={<Avatar size={50} src={satellite} />}
-                                        title={<p>{item.satname}</p>}
-                                        description={`Launch Date: ${item.launchDate}`}
-                                    />
-
-                                </List.Item>
-                            )}
-                        />
-                }
-            </div>
-        );
-    }
+        {isLoad ? (
+          <div className="spin-box">
+            <Spin tip="Loading..." size="large" />
+          </div>
+        ) : (
+          <List
+            className="sat-list"
+            itemLayout="horizontal"
+            size="small"
+            dataSource={satList}
+            renderItem={(item) => (
+              <List.Item
+                actions={[
+                  <Checkbox dataInfo={item} onChange={this.onChange} />,
+                ]}
+              >
+                <List.Item.Meta
+                  avatar={<Avatar size={50} src={satellite} />}
+                  title={<p>{item.satname}</p>}
+                  description={`Launch Date: ${item.launchDate}`}
+                />
+              </List.Item>
+            )}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default SatelliteList;
